69a228c2e49aae3bef9befe70c53236e61357c9f
liurongqiang <liurongqianghello@163.com>
2022-03-06 22:14:25 +0800

    初始化工程

--- src/main/java/com/liurq/handler/DefaultPolicyHandler.java
+++ src/main/java/com/liurq/handler/DefaultPolicyHandler.java
@@ -2,13 +2,11 @@ package com.liurq.handler;
 
 
 import com.liurq.executor.LiurqThreadPoolExecutor;
-import lombok.extern.slf4j.Slf4j;
 
 
 /**
  *
  */
-@Slf4j
 public class DefaultPolicyHandler implements PolicyHandler {
 
     public DefaultPolicyHandler() {
@@ -22,7 +20,7 @@ public class DefaultPolicyHandler implements PolicyHandler {
      */
     @Override
     public void rejected(Runnable task, LiurqThreadPoolExecutor executor) {
-        log.info("任务队列已满..");
+        //TODO 拒绝策略，自行实现
 //        throw new PolicyException("任务已经满了");
     }
 }
