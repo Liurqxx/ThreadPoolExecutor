79f8866d126de1a77ce81b9f3c9440cec881e7a6
liurongqiang <liurongqianghello@163.com>
2022-03-07 17:59:45 +0800

    编写测试

---
+++ src/main/test/com/liurq/test/LiurqThreadTest.java
@@ -0,0 +1,19 @@
+package com.liurq.test;
+
+import org.junit.Test;
+
+/**
+ * @Desc 线程池测试
+ * @Author Liurq
+ * @Date 2022/3/7 17:56
+ */
+public class LiurqThreadTest {
+
+
+    @Test
+    public void threadTestStep01() {
+
+
+    }
+
+}
